ee59e208db720f9603af69dcaca0c96e94764376
ntumbab <bertrand.ntumba@zanaris.com>
2025-09-02 11:26:09 -0400

    init cloudbuild

--- terraform/function.tf
+++ terraform/function.tf
@@ -6,7 +6,7 @@ resource "google_storage_bucket" "function_bucket" {
 resource "google_storage_bucket_object" "function_archive" {
   name   = "function.zip"
   bucket = google_storage_bucket.function_bucket.name
-  source = "../backend/function.zip"
+  source = "../function/function.zip"
 }
 
 resource "google_cloudfunctions2_function" "helloHttp" {
