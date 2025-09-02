862f4341afba6b0d524e161c73f866d89d86702d
ntumbab <bertrand.ntumba@zanaris.com>
2025-09-02 19:43:18 -0400

    remove bucket data source

--- terraform/function.tf
+++ terraform/function.tf
@@ -2,9 +2,6 @@ resource "google_storage_bucket" "function_bucket" {
   name     = "${var.project_id}-function-bucket"
   location = var.region
 }
-data "google_storage_bucket" "function_bucket" {
-  name = "${var.project_id}-function-bucket"
-}
 
 resource "google_storage_bucket_object" "function_archive" {
   name   = "function.zip"
