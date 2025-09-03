73e60547f39a68b9cb954bbfba3e1734f4f5c70c
ntumbab <bertrand.ntumba@zanaris.com>
2025-09-02 22:22:25 -0400

    typo

--- terraform/data.tf
+++ terraform/data.tf
@@ -6,3 +6,12 @@ data "google_project" "my_project" {
 output "project_name" {
   value = data.google_project.my_project.name
 }
+
+data "google_storage_bucket" "function_bucket" {
+  name = "${var.project_id}-function-bucket"
+  location = var.region
+}
+
+output "bucket_name" {
+  value = data.google_storage_bucket.function_bucket.name
+}
